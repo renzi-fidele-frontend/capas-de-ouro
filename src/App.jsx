2d7d6988cb2f8fe3f232422e933e3446e8d4ebc5
Renzi Fidele <renzifidele2001@gmail.com>
2023-09-19 02:39:52 +0200

    Problema de renderização corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,24 @@ import OculosBeleza from "./Pages/OculosBeleza/OculosBeleza";
 import SmoothScroll from "./Components/ScrollTop/ScrollTop";
 
 function App() {
-   <div className="App">
-      <Router>
-         <Header />
-         <SmoothScroll>
-            <Routes>
-               <Route exact path="/" element={<Home />} />
-               <Route path="/contato" element={<Contato />} />
-               <Route path="/comprar/beleza-oculos" element={<Comprar />} />
-               <Route path="/sobre" element={<Sobre />} />
-               <Route path="/politica-de-vendas" element={<TrocasDevolucoes />} />
-               <Route path="/oculos-beleza" element={<OculosBeleza />} />
-            </Routes>
-         </SmoothScroll>
-         <Footer />
-      </Router>
-   </div>;
+   return (
+      <div className="App">
+         <Router>
+            <Header />
+            <SmoothScroll>
+               <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route path="/contato" element={<Contato />} />
+                  <Route path="/comprar/beleza-oculos" element={<Comprar />} />
+                  <Route path="/sobre" element={<Sobre />} />
+                  <Route path="/politica-de-vendas" element={<TrocasDevolucoes />} />
+                  <Route path="/oculos-beleza" element={<OculosBeleza />} />
+               </Routes>
+            </SmoothScroll>
+            <Footer />
+         </Router>
+      </div>
+   );
 }
 
 export default App;
