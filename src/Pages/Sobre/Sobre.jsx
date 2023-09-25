1064581107ad249dfbb246dc2bebe98d661bb7f7
Renzi Fidele <renzifidele2001@gmail.com>
2023-09-25 20:06:48 +0200

    Avancando

--- src/Pages/Sobre/Sobre.jsx
+++ src/Pages/Sobre/Sobre.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Sobre.module.css";
 import estiloHome from "../Home/Home.module.css";
+import loja from "../../assets/loja2.jpg";
 
 const Sobre = () => {
    return (
@@ -14,17 +15,17 @@ const Sobre = () => {
          </p>
          <br />
          <p>
-            A Capas Cardoso Deluxe, nosso primeiro e único modelo, está movimentando o mercado em direção a modelos de qualidade e acessíveis,
+            A Capa Cardoso Deluxe, nosso primeiro e único modelo, está movimentando o mercado em direção a modelos de qualidade e acessíveis,
             servindo de inspiração para várias outras marcas que, aos poucos, começam a seguir este movimento de mudança que seguimos liderando
-            em vendas. Atualmente, a Capas Cardoso Deluxe já é uma capa mais bem avaliado do Brasil e conta com uma média de satisfação 5,0, a melhor
-            média entre todas as empresas do nosso mercado.
+            em vendas. Atualmente, a Capas Cardoso Deluxe já é uma capa mais bem avaliado do Brasil e conta com uma média de satisfação 5,0, a
+            melhor média entre todas as empresas do nosso mercado.
          </p>
          <br />
          <p>
             Na Capas Cardoso Deluxe, nós trabalhamos todos os dias para nos orgulhar do que estamos criando. Tudo que nós fazemos é pensado para
             entregar a melhor experiência possível, o foco em excelência faz parte do nosso DNA e está presente em tudo que nós fazemos.
          </p>
-         <img src={""} alt="Loja fisica" />
+         <img src={loja} alt="Loja fisica" />
       </div>
    );
 };
